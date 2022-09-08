9684c10e9da3ad628e0fbd19dcc95f42c985e314
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-09-08 21:38:49 +0430

    add bash build scripts, add grafana default dashboards

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-docker-compose -f redis-service/docker-compose.yml --env-file ../.env up -d --build
-docker-compose -f postgres-service/docker-compose.yml --env-file ../.env up -d --build
-docker-compose -f exporters/cadvisor/docker-compose.yml --env-file ../.env up -d --build
-docker-compose -f exporters/node-exporter/docker-compose.yml --env-file ../.env up -d --build
-docker-compose -f prometheus-service/docker-compose.yml --env-file ../.env up -d --build
-docker-compose -f grafana-service/docker-compose.yml --env-file ../.env up -d --build
+bash cadvisor/build.sh
+bash grafana/build.sh
+bash prometheus/build.sh
+bash node-exporter/build.sh
+bash portainer/build.sh
+bash redis/build.sh
+bash cadvisor/build.sh
