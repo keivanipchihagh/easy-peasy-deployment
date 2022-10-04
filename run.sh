6e6c745f523069862eab4e64f4ffb1d7149f544e
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-10-04 19:27:25 +0000

    major update

--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-bash cadvisor/build.sh
-bash grafana/build.sh
-bash prometheus/build.sh
-bash node-exporter/build.sh
-bash portainer/build.sh
-bash redis/build.sh
-bash cadvisor/build.sh
-bash pgadmin/build.sh
+bash monitoring/build.sh
